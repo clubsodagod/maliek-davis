219e08285c7dd939a52d56a874e8eda2296e15e5
clubsodagod <maliekdavis@outlook.com>
2025-07-03 16:30:23 -0400

    testing revalidate in production

--- app/blog/posts/[slug]/page.tsx
+++ app/blog/posts/[slug]/page.tsx
@@ -3,10 +3,19 @@ import { IBlogPost } from '@/database/models/blog-posts.model';
 import SlugPostModule from './_components/SlugPostModule';
 import { Metadata } from 'next';
 import { serverBlogFetcher, serverGetBlogPostBySlug } from '@/utility/fetchers/blog.server-fetcher';
+import { revalidatePath } from 'next/cache';
 
+
+revalidatePath('/blog/posts/[slug]', 'layout')
 export const dynamic = "force-static";
-export const dynamicParams = false;
-export const revalidate = 360;
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 60
+ 
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true // or false, to 404 on unknown paths
 
 export async function generateStaticParams() {
     const posts = await serverBlogFetcher() as unknown as IBlogPost[];
